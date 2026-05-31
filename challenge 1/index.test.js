be8e510f116e1a058773eb8451ba14aa93622af6
Chad Harper <chadharper811@gmail.com>
2026-05-31 11:32:47 -0500

    fixed test and finished challenge with test passing

--- challenge 1/index.test.js
+++ challenge 1/index.test.js
@@ -2,5 +2,5 @@
 const {getMax1, getMax2} = require("./index");
 
 test("Functions should return same value", () =>{
-    expect(getMax2() == getMax1());
+    expect(getMax1().toString() == getMax2().toString());
 })
