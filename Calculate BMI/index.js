4d212c454587e6892fcd754ec8aec2b79dd7e098
Chad Harper <chadharper811@gmail.com>
2026-06-04 21:51:45 -0500

    My solution psses all tests, submitting and checking best practice next

--- Calculate BMI/index.js
+++ Calculate BMI/index.js
@@ -5,7 +5,16 @@
 
 // My solution code
 function bmi(weight, height) {
-  return "";
+    let bmiValue = weight / (height **2)
+    if (bmiValue <= 18.5) {
+        return "Underweight";
+    } else if (bmiValue <= 25.0) {
+        return "Normal";
+    } else if (bmiValue <= 30.0) {
+        return "Overweight";
+    } else {
+        return "Obese"
+    }
 }
 
 // Best practice Code
