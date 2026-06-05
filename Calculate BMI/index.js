f08d227095a4fd1252d95f59c5566f4e3b716fe7
Chad Harper <chadharper811@gmail.com>
2026-06-04 22:05:02 -0500

    Found and recoded clever solution, my solution was same as best practice solution

--- Calculate BMI/index.js
+++ Calculate BMI/index.js
@@ -3,20 +3,25 @@
 //   return "";
 // }
 
-// My solution code
-function bmi(weight, height) {
-    let bmiValue = weight / (height **2)
-    if (bmiValue <= 18.5) {
-        return "Underweight";
-    } else if (bmiValue <= 25.0) {
-        return "Normal";
-    } else if (bmiValue <= 30.0) {
-        return "Overweight";
-    } else {
-        return "Obese"
-    }
-}
+// My solution code also best practice
+// function bmi(weight, height) {
+//     let bmiValue = weight / (height **2)
+//     if (bmiValue <= 18.5) {
+//         return "Underweight";
+//     } else if (bmiValue <= 25) {
+//         return "Normal";
+//     } else if (bmiValue <= 30) {
+//         return "Overweight";
+//     } else {
+//         return "Obese"
+//     }
+// }
 
-// Best practice Code
+// Clever solution Code
 
+const bmi = (w, h, bmi = w/(h**2)) =>   bmi <= 18.5 ? "Underweight" :
+                                        bmi <= 25 ? "Normal" : 
+                                        bmi <= 30 ? "Overweight" : "Obese";
+
+                                        
 module.exports = { bmi };
