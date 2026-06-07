f071ecd0697e84afc969d35d08e81f2c6f53ab99
Chad Harper <chadharper811@gmail.com>
2026-06-06 23:27:06 -0500

    my solution passes all tests and passes codewars tests, also checked solutions and came up with a better practice version

--- 8 kyu difficulty/Who ate the cookie/index.js
+++ 8 kyu difficulty/Who ate the cookie/index.js
@@ -1,5 +1,29 @@
-function cookie(x){
-  // ...
+// starting code
+// function cookie(x){
+//   // ...
+// }
+
+// My solution code
+// function cookie(input){
+//     if (typeof input === "string") {
+//         return "Who ate the last cookie? It was Zach!"
+//     } else if (typeof input === "number") {
+//       return "Who ate the last cookie? It was Monica!"
+//     } else {
+//         return "Who ate the last cookie? It was the dog!"
+//     }
+// };
+
+// Possible best practice solution
+const typeObj = {
+    "string": "Zach",
+    "number": "Monica"
 }
+
+function cookie(input){
+    let type = typeof input;
+    let who = (typeObj[type]) ? typeObj[type] : "the dog";
+    return `Who ate the last cookie? It was ${who}!`
+};
 
 module.exports = { cookie };
