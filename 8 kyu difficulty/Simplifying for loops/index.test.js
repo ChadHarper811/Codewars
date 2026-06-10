84645777b97b0ac10c500057376f213b0bf5aae2
Chad Harper <chadharper811@gmail.com>
2026-06-10 18:08:49 -0500

    added starting code and tests need to be finished

---
+++ 8 kyu difficulty/Simplifying for loops/index.test.js
@@ -0,0 +1,7 @@
+const { Number } = require("./index");
+
+describe("Tests", () => {
+    test("Function returns number rounded to the 'precision' decimal place", () => {
+        expect((100).times()).toBe()
+    });
+});
